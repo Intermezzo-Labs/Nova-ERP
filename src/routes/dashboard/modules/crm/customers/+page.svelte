3cb6cee0190d31d70afae2b5f261d3ece159595c
krystlc <alex.valle@gmail.com>
2024-12-08 23:53:39 -0500

    fix: typo

--- src/routes/dashboard/modules/crm/customers/+page.svelte
+++ src/routes/dashboard/modules/crm/customers/+page.svelte
@@ -12,7 +12,7 @@
 
 <div class="flex-1 space-y-4 p-8 pt-6">
 	<div class="flex items-center justify-between space-y-2">
-		<h2 class="text-3xl font-bold tracking-tight">Customer</h2>
+		<h2 class="text-3xl font-bold tracking-tight">Customers</h2>
 		<div class="flex items-center space-x-2">
 			<AddCustomerDialog data={data.form} />
 		</div>
