08f78ebff889d5c2e03def6df81a3170df415528
krystlc <alex.valle@gmail.com>
2024-12-14 22:25:57 -0500

    feat: updated main layout implementing example ui from shadcn. improves design coherence

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,18 +1,27 @@
 <script lang="ts">
-	import Sidebar from './_components/Sidebar.svelte';
-	import Header from './_components/Header.svelte';
+	import TeamSwitcher from './_components/team-switcher.svelte';
+	import Search from './_components/search.svelte';
+	import UserNav from './_components/user-nav.svelte';
+	import MainNav from './_components/main-nav.svelte';
+	import ThemeSwitcher from './_components/theme-switcher.svelte';
+	// import DatePickerWithRange from "$lib/registry/new-york/example/date-picker-with-range.svelte";
 
 	let { data, children } = $props();
 </script>
 
-<div class="flex min-h-screen flex-col bg-background text-foreground lg:flex-row">
-	<Sidebar />
-	<main class="flex w-full flex-1 flex-col lg:ml-64">
-		<Header />
-		<div class="flex-1 overflow-y-auto">
-			<div class="mx-auto w-full max-w-7xl p-4 sm:p-6 lg:p-8">
-				{@render children()}
+<div class="flex h-screen flex-col overflow-hidden">
+	<header class="border-b">
+		<div class="flex h-16 items-center px-4">
+			<TeamSwitcher />
+			<MainNav class="mx-6" />
+			<div class="ml-auto flex items-center space-x-4">
+				<Search />
+				<ThemeSwitcher />
+				<UserNav username={data.user?.email ?? 'N/A'} />
 			</div>
 		</div>
+	</header>
+	<main class="flex-1 overflow-auto">
+		{@render children()}
 	</main>
 </div>
