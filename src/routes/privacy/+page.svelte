2cac2570c6fc8c5b69e47b12b7b4272b4f5943b0
krystlc <alex.valle@gmail.com>
2025-01-05 00:46:12 -0500

    feat: added legal disclaimers

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -1,15 +1,112 @@
-<main>
-	<section class="flex-1 space-y-4 overflow-y-auto whitespace-pre-wrap p-4 text-sm">
-		<h1>Privacy Policy</h1>
+<script>
+	import { PUBLIC_DISCORD_URL } from '$env/static/public';
+</script>
+
+<main class="container mx-auto max-w-xl py-16">
+	<section class="legal-doc">
+		<h1>Nova ERP Privacy Policy</h1>
+
+		<p><strong>1. Introduction</strong></p>
 		<p>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vero commodi, quaerat aperiam
-			corporis tempora ad dolore numquam placeat illum explicabo autem, tenetur cumque in unde! Odio
-			sed magnam ut.
+			1.1 This Privacy Policy explains how Nova ERP ("we," "us," or "our") collects, uses, and
+			shares information about you when you use the Nova ERP software and related services
+			(collectively, the "Services").
 		</p>
+
+		<p><strong>2. Information We Collect</strong></p>
+		<p>2.1 **Personal Information:** We may collect personal information from you, such as:</p>
+		<ul>
+			<li>Contact Information: Name, email address, phone number, company name, address.</li>
+			<li>Account Information: Username, password.</li>
+			<li>
+				Usage Data: Information about how you use the Services, such as your activity within the
+				application, the features you use, and the pages you visit.
+			</li>
+		</ul>
 		<p>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vero commodi, quaerat aperiam
-			corporis tempora ad dolore numquam placeat illum explicabo autem, tenetur cumque in unde! Odio
-			sed magnam ut.
+			2.2 **Other Information:** We may also collect other information, such as your IP address,
+			browser type, and device information.
+		</p>
+
+		<p><strong>3. How We Use Your Information</strong></p>
+		<p>3.1 We use the information we collect to:</p>
+		<ul>
+			<li>Provide and maintain the Services.</li>
+			<li>Create and maintain your account.</li>
+			<li>Respond to your inquiries and requests.</li>
+			<li>Improve the Services.</li>
+			<li>Personalize your experience with the Services.</li>
+			<li>Send you marketing communications (with your consent).</li>
+			<li>Comply with legal and regulatory obligations.</li>
+		</ul>
+
+		<p><strong>4. Data Sharing</strong></p>
+		<p>4.1 We may share your information with:</p>
+		<ul>
+			<li>
+				**Service Providers:** Third-party companies that provide services to us, such as data
+				storage, hosting, and customer support.
+			</li>
+			<li>
+				**Business Partners:** Other companies with whom we collaborate to offer you products or
+				services.
+			</li>
+			<li>
+				**Legal and Regulatory Authorities:** To comply with applicable laws and regulations, such
+				as court orders and subpoenas.
+			</li>
+		</ul>
+
+		<p><strong>5. Data Security</strong></p>
+		<p>
+			5.1 We take reasonable measures to protect your information from unauthorized access, use, or
+			disclosure. However, no method of transmission over the internet or method of electronic
+			storage is 100% secure.
+		</p>
+
+		<p><strong>6. Your Rights</strong></p>
+		<p>
+			6.1 You may have certain rights regarding your personal information, such as the right to:
+		</p>
+		<ul>
+			<li>**Access:** Request access to the personal information we hold about you.</li>
+			<li>
+				**Correction:** Request the correction of any inaccurate personal information we hold about
+				you.
+			</li>
+			<li>**Erasure:** Request the deletion of your personal information.</li>
+			<li>
+				**Restriction:** Request the restriction of our processing of your personal information.
+			</li>
+			<li>
+				**Data Portability:** Request the transfer of your personal information to another
+				organization.
+			</li>
+			<li>**Object:** Object to the processing of your personal information.</li>
+			<li>
+				**Withdraw Consent:** Withdraw your consent to the processing of your personal information
+				at any time.
+			</li>
+		</ul>
+
+		<p><strong>7. Children's Privacy</strong></p>
+		<p>
+			7.1 The Services are not intended for use by children under the age of 18. We do not knowingly
+			collect personal information from children under the age of 18.
+		</p>
+
+		<p><strong>8. Changes to this Privacy Policy</strong></p>
+		<p>
+			8.1 We may update this Privacy Policy from time to time. We will notify you of any material
+			changes to this Privacy Policy, such as by posting the updated Privacy Policy on our website
+			or by sending you an email notification.
+		</p>
+
+		<p><strong>9. Contact Us</strong></p>
+		<p>
+			9.1 If you have any questions about this Privacy Policy, please contact us at our <a
+				href={PUBLIC_DISCORD_URL}>Discord</a
+			>.
 		</p>
 	</section>
 </main>
--- src/routes/terms/+page.svelte
+++ src/routes/terms/+page.svelte
@@ -1,15 +1,113 @@
-<main>
-	<section class="flex-1 space-y-4 overflow-y-auto whitespace-pre-wrap p-4 text-sm">
-		<h1>Terms of Service</h1>
+<script lang="ts">
+	import { PUBLIC_DISCORD_URL } from '$env/static/public';
+</script>
+
+<main class="container mx-auto max-w-xl py-16">
+	<section class="legal-doc">
+		<h1>Nova ERP Terms and Conditions</h1>
+
+		<p><strong>1. Introduction</strong></p>
 		<p>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vero commodi, quaerat aperiam
-			corporis tempora ad dolore numquam placeat illum explicabo autem, tenetur cumque in unde! Odio
-			sed magnam ut.
+			1.1 These Terms and Conditions ("Terms") govern your access to and use of the Nova ERP
+			software and related services (collectively, the "Services"). By accessing or using the
+			Services, you ("you" or "User") agree to be bound by these Terms. If you do not agree to these
+			Terms, you may not access or use the Services.
+		</p>
+
+		<p><strong>2. Services</strong></p>
+		<p>
+			2.1 The Services provide you with tools to manage your business operations, including but not
+			limited to inventory tracking, customer data management, and other related functionalities.
 		</p>
 		<p>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab vero commodi, quaerat aperiam
-			corporis tempora ad dolore numquam placeat illum explicabo autem, tenetur cumque in unde! Odio
-			sed magnam ut.
+			2.2 We may update or modify the Services from time to time, including adding new features,
+			removing existing features, and changing the user interface. We will endeavor to provide you
+			with reasonable notice of any material changes.
+		</p>
+
+		<p><strong>3. User Accounts</strong></p>
+		<p>
+			3.1 You are responsible for maintaining the confidentiality of your account credentials and
+			for all activities that occur under your account.
+		</p>
+		<p>
+			3.2 You agree to immediately notify us of any unauthorized use of your account or any other
+			breach of security.
+		</p>
+
+		<p><strong>4. Data Privacy and Security</strong></p>
+		<p>
+			4.1 We will handle your data in accordance with our Privacy Policy, which is incorporated into
+			these Terms by reference. You can find our Privacy Policy here: <a href="/privacy"
+				>Privacy Policy</a
+			>.
+		</p>
+		<p>
+			4.2 We take reasonable measures to protect your data from unauthorized access, use, or
+			disclosure. However, we cannot guarantee the security of your data and cannot be held liable
+			for any unauthorized access or disclosure of your data.
+		</p>
+
+		<p><strong>5. Intellectual Property</strong></p>
+		<p>
+			5.1 The Services and all related intellectual property rights, including but not limited to
+			trademarks, copyrights, and patents, are owned by us or our licensors.
+		</p>
+		<p>
+			5.2 You may not use, copy, modify, distribute, or reproduce any portion of the Services
+			without our prior written consent.
+		</p>
+
+		<p><strong>6. Disclaimer of Warranties</strong></p>
+		<p>
+			6.1 THE SERVICES ARE PROVIDED "AS IS" AND "AS AVAILABLE" WITHOUT WARRANTY OF ANY KIND, EXPRESS
+			OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS
+			FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.
+		</p>
+		<p>
+			6.2 WE DO NOT WARRANT THAT THE SERVICES WILL BE UNINTERRUPTED, ERROR-FREE, OR SECURE, OR THAT
+			ANY DEFECTS WILL BE CORRECTED.
+		</p>
+
+		<p><strong>7. Limitation of Liability</strong></p>
+		<p>
+			7.1 IN NO EVENT SHALL WE BE LIABLE FOR ANY INDIRECT, INCIDENTAL, SPECIAL, CONSEQUENTIAL, OR
+			PUNITIVE DAMAGES, INCLUDING BUT NOT LIMITED TO LOSS OF PROFITS, DATA LOSS, OR BUSINESS
+			INTERRUPTION, ARISING OUT OF OR RELATED TO YOUR USE OF OR INABILITY TO USE THE SERVICES,
+			WHETHER BASED ON WARRANTY, CONTRACT, TORT (INCLUDING NEGLIGENCE), OR ANY OTHER LEGAL THEORY,
+			EVEN IF WE HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+		</p>
+
+		<p><strong>8. Termination</strong></p>
+		<p>
+			8.1 We may terminate or suspend your access to the Services at any time, with or without
+			cause, and with or without prior notice.
+		</p>
+		<p>
+			8.2 You may terminate your use of the Services at any time by letting us know through Discord.
+		</p>
+
+		<p><strong>9. Governing Law</strong></p>
+		<p>
+			9.1 These Terms shall be governed by and construed in accordance with the laws of the United
+			States of America.
+		</p>
+
+		<p><strong>10. Dispute Resolution</strong></p>
+		<p>10.1 Contact us directly.</p>
+
+		<p><strong>11. Changes to these Terms</strong></p>
+		<p>
+			11.1 We may update these Terms from time to time. We will notify you of any material changes
+			to these Terms, such as by posting the updated Terms on our website or by sending you an email
+			notification.
+		</p>
+
+		<p><strong>12. Contact Us</strong></p>
+		<p>
+			12.1 If you have any questions about these Terms, please contact us in our <a
+				href={PUBLIC_DISCORD_URL}>Discord</a
+			> server.
 		</p>
 	</section>
 </main>
